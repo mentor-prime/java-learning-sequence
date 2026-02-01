07aeeefc3622131d7656a982173b19e33f8e963e
Amad Bin Mumtaz <mentor.prime.education@gmail.com>
2026-02-01 01:05:34 +0000

    palindrome function

---
+++ src/main/java/org/example/palind.java
@@ -0,0 +1,47 @@
+package org.example;
+
+public class palind {
+    
+
+    public static void main(String[] args) {
+
+        String tc1 = "vivic";
+        String tc2 = "civic";
+
+        if (checkPalindrome(tc2) == true ) {
+            System.out.println("Palindrome");
+        } else {
+            System.out.println("Not a Palindrome");
+        }
+
+
+    }
+
+
+    public static boolean checkPalindrome(String string) {
+        
+        char[] originalString = string.toCharArray();
+        char[] reversedString = new char[originalString.length];
+
+        for (int i = originalString.length - 1; i >= 0; i--) {
+
+                reversedString[i] = originalString[originalString.length - i -1];
+
+        }
+
+        System.out.println(reversedString);
+        
+
+
+        for(int i = 0; i < originalString.length; i++) {
+            if (reversedString[i] != originalString[i]) {
+                return false;   
+            }
+        }
+
+        return true;
+
+
+    }
+
+}
