1a897f0d4b3b77123b2f34bce77cc9523c27ebf9
mentor-prime <mentor.prime.education@gmail.com>
2026-02-12 19:34:07 -0600

    added new function

---
+++ src/main/java/org/example/palindrome/palindrome4.java
@@ -0,0 +1,24 @@
+package org.example.palindrome;
+
+public class palindrome4 {
+
+    public static void main(String[] args) {
+        String keyword = "12344321";
+        System.out.println(checkPalindrome(keyword) ? "Palindrome" : "NO Palindrome");
+    }
+
+    public static boolean checkPalindrome(String string) {
+        char[] originalString = string.toCharArray();
+        char[] reversedString = new char[originalString.length];
+        for (int i = originalString.length - 1; i >= 0; i--) {
+            reversedString[i] = originalString[i];
+        }
+        System.out.print(reversedString);
+        for (int  i = 0; i < originalString.length; i++) {
+            if (reversedString[i] != originalString[i]) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
