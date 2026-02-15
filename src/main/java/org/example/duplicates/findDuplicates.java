632b87c907856012ee1e07c8a8fef0f5932fd372
mentor-prime <mentor.prime.education@gmail.com>
2026-02-15 03:49:55 -0600

    findDuplicates

---
+++ src/main/java/org/example/duplicates/findDuplicates.java
@@ -0,0 +1,21 @@
+package org.example.duplicates;
+
+public class findDuplicates {
+
+    public static void main(String[] args) {
+        int[] array = {1,2, 2, 1, 2, 3, 4, 5, 5};
+        findDuplicate(array);
+    }
+
+    public static void findDuplicate(int[] array) {
+        for (int  i = 0; i < array.length; i++) {
+            for (int j = i + 1; j < array.length; j++) {
+                if (array[i] == array[j]) {
+                    System.out.println("Duplicate value: " + array[j]);
+                    break;
+                }
+            }
+        }
+    }
+
+}
