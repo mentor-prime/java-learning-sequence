1ab77890801e5e8e8fcf02be82e36825cc6021c0
mentor-prime <mentor.prime.education@gmail.com>
2026-02-22 22:53:26 -0600

    fibonacciSeries

---
+++ src/main/java/org/example/fibonacci/fibonacciSeries3.java
@@ -0,0 +1,22 @@
+package org.example.fibonacci;
+
+public class fibonacciSeries3 {
+
+    public static void main(String[] args) {
+        printSequence(10);
+        printSequence(11);
+    }
+
+    public static void printSequence(int number) {
+        int previous = 0;
+        int current = 1;
+        if (number == 0) {System.out.println(previous );}
+        if (number == 1) {System.out.println(current );}
+        for (int i = 2; i < number; i++) {
+            int next = previous + current;
+            previous = current;
+            current = next;
+            System.out.println(next);
+        }
+    }
+}
