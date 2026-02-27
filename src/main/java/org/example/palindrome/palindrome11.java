83d9a54200c81edea1427cdcfcf2b086242c4c4f
mentor-prime <mentor.prime.education@gmail.com>
2026-02-26 23:22:48 -0600

    palindrome

---
+++ src/main/java/org/example/palindrome/palindrome11.java
@@ -0,0 +1,25 @@
+package org.example.palindrome;
+
+public class palindrome11 {
+
+    public static void main(String[] args) {
+        String word = "123321";
+        System.out.println(checkPalindrome(word) ? "Palindrome" : "Not a palindrome" );
+    }
+
+    public static boolean checkPalindrome(String word) {
+
+        char[] originalString = word.toCharArray();
+        char[] reversedString = new char[originalString.length];
+        for (int i = 0; i < originalString.length; i++) {
+            reversedString[i] = originalString[originalString.length - 1 - i];
+        }
+        System.out.println(reversedString);
+        for (int i = 0; i < originalString.length; i ++) {
+            if (reversedString[i] != originalString[i]) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
