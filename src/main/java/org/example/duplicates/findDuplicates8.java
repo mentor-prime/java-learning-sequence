6db96f5b28aa92cd62ddfad2a30f5dfcf61eebef
mentor-prime <mentor.prime.education@gmail.com>
2026-03-01 23:35:12 -0600

    findDuplicates

--- src/main/java/org/example/duplicates/findDuplicates8.java
+++ src/main/java/org/example/duplicates/findDuplicates8.java
@@ -1,22 +1,17 @@
 package org.example.duplicates;
 
-public class findDuplicates7 {
+public class findDuplicates8 {
 
     public static void main(String[] args) {
         int[] intArray = {1, 2, 2, 1, 2, 3, 4, 5, 5};
-        String[] stringArray = {"a", "b", "a", "d", "c"};
-
         findDuplicatesInt(intArray);
-    //    findDuplicateString(stringArray);
     }
 
     public static void findDuplicatesInt(int[] array) {
-        System.out.println("Duplicate ints:");
         for (int i = 0; i < array.length; i++) {
             for (int j = i + 1; j < array.length; j++) {
                 if (array[i] == array[j]) {
-                    System.out.println(array[j]);
-                    break;
+                    System.out.println("Duplicate value at index:" + j + " is " + array[j]);
                 }
             }
         }
