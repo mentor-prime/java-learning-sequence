ed230ae16f32d2096d5671b2e5778abbc55027dd
mentor-prime <mentor.prime.education@gmail.com>
2026-03-08 23:28:34 -0500

    reverseString

---
+++ src/main/java/org/example/stringReverse/reverseString5.java
@@ -0,0 +1,19 @@
+package org.example.stringReverse;
+
+public class reverseString5 {
+
+    public static void main(String[] args) {
+        String string = "this is a string";
+        System.out.println("Reversed string: " + reverseFunction(string));
+    }
+
+    public static String reverseFunction(String string) {
+        char[] array = string.toCharArray();
+        char[] reversedArray = new char[array.length];
+        for (int i = array.length -1; i >= 0; i--) {
+            reversedArray[array.length - i - 1] = array[i];
+        }
+        return new String(reversedArray);
+    }
+
+}
