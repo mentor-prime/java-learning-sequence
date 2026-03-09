3ac44630fc5155320936200bab591dba1e9efa12
mentor-prime <mentor.prime.education@gmail.com>
2026-03-09 15:46:40 -0500

    reversedString

--- src/main/java/org/example/stringReverse/reverseString6.java
+++ src/main/java/org/example/stringReverse/reverseString6.java
@@ -8,15 +8,11 @@ public class reverseString6 {
     }
 
     public static String reverseFunction(String string) {
-
         char[] originalString = string.toCharArray();
         char[] reversedString = new char[originalString.length];
-
         for (int i = 0; i < originalString.length; i++) {
             reversedString[i] = originalString[originalString.length -1 - i];
         }
-
         return new String(reversedString);
     }
-
 }
