d44acc68611760f6d99c05d607ddea2919fcfd68
mentor-prime <mentor.prime.education@gmail.com>
2026-03-08 23:36:51 -0500

    reduced comments in OOP code

--- src/main/java/org/example/OOP/abstraction/calculator/BasicCalculator.java
+++ src/main/java/org/example/OOP/abstraction/calculator/BasicCalculator.java
@@ -2,13 +2,11 @@ package org.example.OOP.abstraction.calculator;
 
 class BasicCalculator extends Calculator {
 
-    // Implementation of add method
     @Override
     int add(int a, int b) {
         return a + b;
     }
 
-    // Implementation of subtract method
     @Override
     int subtract(int a, int b) {
         return a - b;
