656542d9752516666705558b0fc13b1912af170b
mentor-prime <mentor.prime.education@gmail.com>
2026-03-10 13:51:32 -0500

    reverseString

---
+++ src/main/java/org/example/stringReverse/reverseString7.java
@@ -0,0 +1,17 @@
+package org.example.stringReverse;
+
+public class reverseString7 {
+    public static void main(String[] args) {
+        String string = "string";
+        System.out.println("Reversed string: " + reverseFunction(string));
+    }
+
+    public static String reverseFunction(String string) {
+        char[] originalString = string.toCharArray();
+        char[] reversedString = new char[originalString.length];
+        for (int i = 0; i < originalString.length; i++) {
+            reversedString[i] = originalString[originalString.length -1 - i];
+        }
+        return new String(reversedString);
+    }
+}
