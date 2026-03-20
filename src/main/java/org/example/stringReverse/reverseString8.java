06f326286deea9546445c82cdfdc6d8495697526
mentor-prime <mentor.prime.education@gmail.com>
2026-03-19 21:48:11 -0500

    ReverseString

---
+++ src/main/java/org/example/stringReverse/reverseString8.java
@@ -0,0 +1,19 @@
+package org.example.stringReverse;
+
+import java.util.Arrays;
+
+public class reverseString8 {
+    public static int[] reverseArray(int[] arr) {
+        int[] reversedArray = new int[arr.length];
+        for (int i = 0; i < arr.length; i++) {
+            reversedArray[i] = arr[arr.length - i - 1];
+        }
+        return reversedArray;
+    }
+
+    public static void main(String[] args) {
+        int[] array = {1, 2, 3, 4, 5};
+        int[] result = reverseArray(array);
+        System.out.println("Reversed array: " + Arrays.toString(result));
+    }
+}
